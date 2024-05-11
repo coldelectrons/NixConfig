24a6d69166258b2f413a527f24fa45861ba48255
coldelectrons <frithomas@gmail.com>
2024-05-11 12:23:59 -0400

    added megamind

---
+++ home/coldelectrons/megamind.nix
@@ -0,0 +1,28 @@
+{ config, lib, pkgs, inputs, ... }:
+
+{
+  imports = [
+    ./global
+    ./features/theme
+    ./features/desktop/kde/plasma6
+    ./features/games
+    ./features/lunarvim
+    ./features/cli
+    ./features/productivity
+    ./features/virt-manager
+    ./features/cli
+  ];
+
+  ### Special Variables
+  variables.useVR = true;
+  variables.useKonsole = false;
+  variables.machine.gpu = "amd";
+  variables.desktop.displayManager = "wayland";
+  # variables.machine.motherboard = "amd";
+  variables.machine.buildType = "desktop";
+  # variables.machine.lowSpec = false;
+  ###
+
+  # Install a couple more packages
+  home.packages = with pkgs; [ krita kdePackages.tokodon ];
+}
