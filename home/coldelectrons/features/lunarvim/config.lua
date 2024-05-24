014006e54b6ea09a068d52bcc4bed1cc34db14b7
coldelectrons <frithomas@gmail.com>
2024-05-24 17:59:48 -0400

    lunarvim -swenv

--- home/coldelectrons/features/lunarvim/config.lua
+++ home/coldelectrons/features/lunarvim/config.lua
@@ -394,7 +394,7 @@ lvim.plugins = {
 		-- vim.cmd [[packadd telescope.nvim]]
 		-- end,
 	},
-	{ "Vigemus/iron.nvim" },
+	-- { "Vigemus/iron.nvim" },
 	-- { "akinsho/toggleterm.nvim", version = "*", config = true },
 	-- { "voldikss/vim-floaterm" },
 	-- {
@@ -443,64 +443,64 @@ require("gui-font-resize").setup({default_size=10,change_by=1,bounds={maximum=20
 --   enable_suggestions_on_files = {"*.py", "*.rs", "*.cpp", "*.h", "*.c"},
 -- })
 
-lvim.builtin.which_key.mappings["C"] = {
-	name = "Python",
-	c = { "<cmd>lua require('swenv.api').pick_venv()<cr>", "Choose Env" },
-}
+-- lvim.builtin.which_key.mappings["C"] = {
+-- 	name = "Python",
+-- 	c = { "<cmd>lua require('swenv.api').pick_venv()<cr>", "Choose Env" },
+-- }
 
-require("swenv").setup({
-	post_set_venv = function()
-		vim.cmd("LspRestart")
-	end,
-})
+-- require("swenv").setup({
+-- 	post_set_venv = function()
+-- 		vim.cmd("LspRestart")
+-- 	end,
+-- })
 
-local iron = require("iron.core")
-iron.setup({
-	config = {
-		-- Whether a repl should be discarded or not
-		scratch_repl = false,
-		-- Your repl definitions come here
-		repl_definition = {
-			sh = {
-				-- Can be a table or a function that
-				-- returns a table (see below)
-				command = { "zsh" },
-			},
-		},
-		-- How the repl window will be displayed
-		-- See below for more information
-		repl_open_cmd = require("iron.view").bottom(40),
-	},
-	-- Iron doesn't set keymaps by default anymore.
-	-- You can set them here or manually add keymaps to the functions in iron.core
-	-- keymaps = {
-	--   send_motion = "<space>sc",
-	--   visual_send = "<space>sc",
-	--   send_file = "<space>sf",
-	--   send_line = "<space>sl",
-	--   send_until_cursor = "<space>su",
-	--   send_mark = "<space>sm",
-	--   mark_motion = "<space>mc",
-	--   mark_visual = "<space>mc",
-	--   remove_mark = "<space>md",
-	--   cr = "<space>s<cr>",
-	--   interrupt = "<space>s<space>",
-	--   exit = "<space>sq",
-	--   clear = "<space>cl",
-	-- },
-	-- If the highlight is on, you can change how it looks
-	-- For the available options, check nvim_set_hl
-	highlight = {
-		italic = true,
-	},
-	ignore_blank_lines = true, -- ignore blank lines when sending visual select lines
-})
+-- local iron = require("iron.core")
+-- iron.setup({
+-- 	config = {
+-- 		-- Whether a repl should be discarded or not
+-- 		scratch_repl = false,
+-- 		-- Your repl definitions come here
+-- 		repl_definition = {
+-- 			sh = {
+-- 				-- Can be a table or a function that
+-- 				-- returns a table (see below)
+-- 				command = { "zsh" },
+-- 			},
+-- 		},
+-- 		-- How the repl window will be displayed
+-- 		-- See below for more information
+-- 		repl_open_cmd = require("iron.view").bottom(40),
+-- 	},
+-- 	-- Iron doesn't set keymaps by default anymore.
+-- 	-- You can set them here or manually add keymaps to the functions in iron.core
+-- 	-- keymaps = {
+-- 	--   send_motion = "<space>sc",
+-- 	--   visual_send = "<space>sc",
+-- 	--   send_file = "<space>sf",
+-- 	--   send_line = "<space>sl",
+-- 	--   send_until_cursor = "<space>su",
+-- 	--   send_mark = "<space>sm",
+-- 	--   mark_motion = "<space>mc",
+-- 	--   mark_visual = "<space>mc",
+-- 	--   remove_mark = "<space>md",
+-- 	--   cr = "<space>s<cr>",
+-- 	--   interrupt = "<space>s<space>",
+-- 	--   exit = "<space>sq",
+-- 	--   clear = "<space>cl",
+-- 	-- },
+-- 	-- If the highlight is on, you can change how it looks
+-- 	-- For the available options, check nvim_set_hl
+-- 	highlight = {
+-- 		italic = true,
+-- 	},
+-- 	ignore_blank_lines = true, -- ignore blank lines when sending visual select lines
+-- })
 
--- iron also has a list of commands, see :h iron-commands for all available commands
-vim.keymap.set("n", "<space>rs", "<cmd>IronRepl<cr>")
-vim.keymap.set("n", "<space>rr", "<cmd>IronRestart<cr>")
-vim.keymap.set("n", "<space>rf", "<cmd>IronFocus<cr>")
-vim.keymap.set("n", "<space>rh", "<cmd>IronHide<cr>")
+-- -- iron also has a list of commands, see :h iron-commands for all available commands
+-- vim.keymap.set("n", "<space>rs", "<cmd>IronRepl<cr>")
+-- vim.keymap.set("n", "<space>rr", "<cmd>IronRestart<cr>")
+-- vim.keymap.set("n", "<space>rf", "<cmd>IronFocus<cr>")
+-- vim.keymap.set("n", "<space>rh", "<cmd>IronHide<cr>")
 
 local opts = { noremap = true, silent = true }
 
