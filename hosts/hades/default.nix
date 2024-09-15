684c8f8f0c1244992b90abb6e20f80ad1b4d0938
coldelectrons <frithomas@gmail.com>
2024-09-15 13:02:28 -0400

    misc unnotable

--- hosts/hades/default.nix
+++ hosts/hades/default.nix
@@ -47,7 +47,7 @@
   # (the default) this is the recommended approach. When using systemd-networkd it's
   # still possible to use this option, but it's recommended to use it in conjunction
   # with explicit per-interface declarations with `networking.interfaces.<interface>.useDHCP`.
-  networking.useDHCP = lib.mkDefault true;
+  networking.useDHCP = lib.mkDefault false;
   # networking.interfaces.wlp0s20f3.useDHCP = lib.mkDefault true;
 
   # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
