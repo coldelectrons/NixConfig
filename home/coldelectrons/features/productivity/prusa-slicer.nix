514e011888328ddce6b55f28ea4af1bbe391f7c3
coldelectrons <frithomas@gmail.com>
2024-09-22 17:12:29 -0400

    move away from home packages

--- home/coldelectrons/features/productivity/prusa-slicer.nix
+++ home/coldelectrons/features/productivity/prusa-slicer.nix
@@ -1,48 +1,48 @@
 { config, lib, pkgs, ... }:
 
 {
-  xdg = {
-    mimeApps = {
-      associations = {
-        added = {
-          "x-scheme-handler/prusaslicer" = "PrusaSlicerURLProtocol.desktop";
-          "model/stl" = "PrusaSlicer.desktop";
-        };
-      };
-      defaultApplications = {
-        "x-scheme-handler/prusaslicer" = "PrusaSlicerURLProtocol.desktop";
-        "model/stl" = "PrusaSlicer.desktop";
-      };
-    };
+  # xdg = {
+  #   mimeApps = {
+  #     associations = {
+  #       added = {
+  #         "x-scheme-handler/prusaslicer" = "PrusaSlicerURLProtocol.desktop";
+  #         "model/stl" = "PrusaSlicer.desktop";
+  #       };
+  #     };
+  #     defaultApplications = {
+  #       "x-scheme-handler/prusaslicer" = "PrusaSlicerURLProtocol.desktop";
+  #       "model/stl" = "PrusaSlicer.desktop";
+  #     };
+  #   };
 
-    configFile = {
-      "PrusaSlicer/printer" = {
-        source = config.lib.file.mkOutOfStoreSymlink
-          "${config.home.sessionVariables.FLAKE}/home/coldelectrons/dots/PrusaSlicer/printer";
-        recursive = true;
-      };
-      "PrusaSlicer/print" = {
-        source = config.lib.file.mkOutOfStoreSymlink
-          "${config.home.sessionVariables.FLAKE}/home/coldelectrons/dots/PrusaSlicer/print";
-        recursive = true;
-      };
-      "PrusaSlicer/physical_printer" = {
-        source = config.lib.file.mkOutOfStoreSymlink
-          "${config.home.sessionVariables.FLAKE}/home/coldelectrons/dots/PrusaSlicer/physical_printer";
-        recursive = true;
-      };
-      "PrusaSlicer/filament" = {
-        source = config.lib.file.mkOutOfStoreSymlink
-          "${config.home.sessionVariables.FLAKE}/home/coldelectrons/dots/PrusaSlicer/filament";
-        recursive = true;
-      };
-      "PrusaSlicer/bed_models" = {
-        source = config.lib.file.mkOutOfStoreSymlink
-          "${config.home.sessionVariables.FLAKE}/home/coldelectrons/dots/PrusaSlicer/bed_models";
-        recursive = true;
-      };
-    };
-  };
+  #   configFile = {
+  #     "PrusaSlicer/printer" = {
+  #       source = config.lib.file.mkOutOfStoreSymlink
+  #         "${config.home.sessionVariables.FLAKE}/home/coldelectrons/dots/PrusaSlicer/printer";
+  #       recursive = true;
+  #     };
+  #     "PrusaSlicer/print" = {
+  #       source = config.lib.file.mkOutOfStoreSymlink
+  #         "${config.home.sessionVariables.FLAKE}/home/coldelectrons/dots/PrusaSlicer/print";
+  #       recursive = true;
+  #     };
+  #     "PrusaSlicer/physical_printer" = {
+  #       source = config.lib.file.mkOutOfStoreSymlink
+  #         "${config.home.sessionVariables.FLAKE}/home/coldelectrons/dots/PrusaSlicer/physical_printer";
+  #       recursive = true;
+  #     };
+  #     "PrusaSlicer/filament" = {
+  #       source = config.lib.file.mkOutOfStoreSymlink
+  #         "${config.home.sessionVariables.FLAKE}/home/coldelectrons/dots/PrusaSlicer/filament";
+  #       recursive = true;
+  #     };
+  #     "PrusaSlicer/bed_models" = {
+  #       source = config.lib.file.mkOutOfStoreSymlink
+  #         "${config.home.sessionVariables.FLAKE}/home/coldelectrons/dots/PrusaSlicer/bed_models";
+  #       recursive = true;
+  #     };
+  #   };
+  # };
 
-  home.packages = with pkgs; [ prusa-slicer ];
+  # home.packages = with pkgs; [ prusa-slicer ];
 }
